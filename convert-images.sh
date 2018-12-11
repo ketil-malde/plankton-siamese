32f953ca73e383d688fa362aa86fb972d9cbbe12
Ketil Malde <ketil@malde.org>
2018-12-11 17:38:18 +0100

    Add convert-images.sh to rescale data to 299x299 pixels

---
+++ convert-images.sh
@@ -0,0 +1,18 @@
+
+# safety first
+set -e -o pipefail
+shopt -s failglob 
+
+# resize to 299x299, but keep image size
+# drop -resize if zooming with crop is better (but maybe not?)
+
+BASE=ZooScanSet/imgs
+OUT=data
+
+for dir in $(ls $BASE); do
+   echo $dir
+   mkdir -p $OUT/$dir
+   ls $BASE/$dir | while read f; do 
+          convert -resize 299x299 $BASE/$dir/$f -background white -gravity center -extent 299x299 $OUT/$dir/$f
+   done
+done
