dab0a0931e944657ef177654bd677f4022b615a4
Ketil Malde <ketil@malde.org>
2019-01-29 10:40:05 +0100

    Comment out print statements

--- generators.py
+++ generators.py
@@ -29,23 +29,21 @@ images = [os.listdir(os.path.join('train',x)) for x in classes]
 def mk_triplet():
     # pick random positive class
     pos_class = random.randint(0,len(classes)-1)
-    # pick random, different negative class
-    print('Anchor: ',pos_class,classes[pos_class])
+    # print('Anchor: ',pos_class,classes[pos_class])
 
+    # pick random, different negative class
     neg_class = random.randint(0,len(classes)-2)
     if neg_class >= pos_class:
         neg_class = neg_class + 1
-    print('Negative: ',neg_class,classes[neg_class])
+    # print('Negative: ',neg_class,classes[neg_class])
 
     # pick two random images from class
     anchor = os.path.join('train',classes[pos_class],random.choice(images[pos_class]))
     pos = os.path.join('train',classes[pos_class],random.choice(images[pos_class]))
     neg = os.path.join('train',classes[neg_class],random.choice(images[neg_class]))
 
-    print('Selection:',anchor,pos,neg)
+    # print('Selection:',anchor,pos,neg)
     return(pos_class,neg_class,anchor,pos,neg)
-
-print(mk_triplet())
 
 from PIL import Image
 import numpy as np
@@ -55,7 +53,7 @@ def triplet_generator(batch_size):
     ans = []
     pss = []
     ngs = []
-    for i in range(0,batch_size):
+    for i in range(0,batch_size-1):
         pc,nc,anc,pos,neg = mk_triplet()
         ys.append((pc,nc))
         ans.append(np.array(Image.open(anc))/256)
@@ -69,5 +67,7 @@ def triplet_generator(batch_size):
 
     yield(ys,a,p,n)
 
-# for x in triplet_generator(1):
-#    print(x)
+for x in triplet_generator(32):
+    #    print(x)
+    print()
+    
