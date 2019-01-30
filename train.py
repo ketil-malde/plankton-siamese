33b1a52ac0d615ba4b3926d31d05093930bd465e
Ketil Malde <ketil@malde.org>
2019-01-30 09:05:51 +0100

    Fix imports, update to match new generator interface

--- train.py
+++ train.py
@@ -1,14 +1,12 @@
 # The imported generators expect to find training data in data/train
 # and validation data in data/validation
-from generators import triplet_generator
-
 from keras.models import load_model
 from keras.callbacks import CSVLogger
-from keras.optimizers import SGD
 
 from create_model import model
+from generators import triplet_generator
 
-from config import batch_size
+import config as C
 
 last = 0
 
@@ -20,9 +18,9 @@ logger = CSVLogger('train.log', append=True, separator='\t')
 
 def train_step(i):
     model.fit_generator(
-        triplet_generator(batch_size), steps_per_epoch=1000, epochs=10,
+        triplet_generator(batch_size, None, C.train_dir), steps_per_epoch=1000, epochs=10,
         callbacks=[logger],
-        validation_data=triplet_generator(batch_size), validation_steps=500)
+        validation_data=triplet_generator(batch_size, None, C.val_dir), validation_steps=500)
     model.save(save_name(i))
 
 
