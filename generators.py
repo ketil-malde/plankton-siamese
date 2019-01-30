3ebe23f92d527e0d2a32a55d7a8749fa3741c1bb
Ketil Malde <ketil@malde.org>
2019-01-30 11:26:03 +0100

    Make mk_triplets a generator too, more efficient

--- generators.py
+++ generators.py
@@ -2,27 +2,30 @@ import os
 import random
 import config as C
 
-def mk_triplet(directory):
+
+def mk_triplets(directory):
+
     classes = os.listdir(directory)
     images = [os.listdir(os.path.join(directory,x)) for x in classes]
 
-    # pick random positive class
-    pos_class = random.randint(0,len(classes)-1)
-    # print('Anchor: ',pos_class,classes[pos_class])
+    while True:
+        # pick random positive class
+        pos_class = random.randint(0,len(classes)-1)
+        # print('Anchor: ',pos_class,classes[pos_class])
 
-    # pick random, different negative class
-    neg_class = random.randint(0,len(classes)-2)
-    if neg_class >= pos_class:
-        neg_class = neg_class + 1
-    # print('Negative: ',neg_class,classes[neg_class])
+        # pick random, different negative class
+        neg_class = random.randint(0,len(classes)-2)
+        if neg_class >= pos_class:
+            neg_class = neg_class + 1
+        # print('Negative: ',neg_class,classes[neg_class])
 
-    # pick two random images from class
-    anchor = os.path.join(directory, classes[pos_class], random.choice(images[pos_class]))
-    pos    = os.path.join(directory, classes[pos_class], random.choice(images[pos_class]))
-    neg    = os.path.join(directory, classes[neg_class], random.choice(images[neg_class]))
+        # pick two random images from class
+        anchor = os.path.join(directory, classes[pos_class], random.choice(images[pos_class]))
+        pos    = os.path.join(directory, classes[pos_class], random.choice(images[pos_class]))
+        neg    = os.path.join(directory, classes[neg_class], random.choice(images[neg_class]))
 
-    # print('Selection:',anchor,pos,neg)
-    return(pos_class,neg_class,anchor,pos,neg)
+        # print('Selection:',anchor,pos,neg)
+        yield (pos_class,neg_class,anchor,pos,neg)
 
 from PIL import Image
 import numpy as np
@@ -40,13 +43,14 @@ def paste(img):
     return i
 
 def triplet_generator(batch_size,cache_size,directory):
+    trips = mk_triplets(directory)
     while True:
         ys = []
         ans = []
         pss = []
         ngs = []
         for i in range(0,batch_size):
-            pc,nc,anc,pos,neg = mk_triplet(directory)
+            pc,nc,anc,pos,neg = next(trips)
             ys.append((pc,nc))
             a_img = np.array(Image.open(anc))/256
             p_img = np.array(Image.open(pos))/256
@@ -64,7 +68,8 @@ def triplet_generator(batch_size,cache_size,directory):
 
         yield [a,p,n], y
 
-# Testing:    
+# Testing:
+print("### Testing triplet_generator ###")
 g = triplet_generator(4, None, C.train_dir)
 for x in range(0,4):
     [a,p,n], y = next(g)
