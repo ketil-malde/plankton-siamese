555d7b88698b436e1edf2e694dfacc9289568393
Ketil Malde <ketil@malde.org>
2019-03-07 07:50:53 +0100

    Fixes, plus add centroid movement output

--- train.py
+++ train.py
@@ -43,6 +43,8 @@ model = tripletize(base_model)
 model.compile(optimizer=SGD(lr=C.learn_rate, momentum=0.9),
               loss=std_triplet_loss())
 
+cents = [T.centroid(v) for v in T.get_vectors(base_model, C.val_dir)]
+
 for i in range(last+1, last+11):
     log('Starting iteration '+str(i)+' lr='+str(C.learn_rate))
     train_step()
@@ -58,3 +60,7 @@ for i in range(last+1, last+11):
         T.summarize(vs, outfile=sumfile)
     with open('clusters.'+str(i)+'.log', 'w') as cfile:
         T.confusion_counts(c, outfile=cfile)
+    c_tmp = [T.centroid(v) for v in T.get_vectors(base_model, C.val_dir)]
+    c_mv = [T.dist(a,b) for a in cents for b in c_tmp]
+    log('Centroid moved: '+str(c_mv))
+    cents = c_tmp
